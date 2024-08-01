21aac92e28f146fc3d8e472bd0c595deaf2084ce
Matt Auer <mattauer@umich.edu>
2024-08-01 11:14:11 -0500

    try to get multiple domains to work

--- nft/vite.config.ts
+++ nft/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,7 +8,22 @@ export default defineConfig({
   build: {
     outDir: process.env.NFT_DIST,
   },
-  plugins: [react()],
+  plugins: [react(), vercel()],
+  // @ts-ignore
+  vercel: {
+    rewrites: [
+      {
+        "source": "/nft/:path*",
+        "destination": "/nft/create/:path*",
+        "has": [
+          {
+            "type": "host",
+            "value": "sway-nft.vercel.app/(.*)"
+          }
+        ]
+      }
+    ]
+  },
   resolve: {
     alias: {
       src: "/src",
