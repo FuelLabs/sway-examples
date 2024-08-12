f457a9df5e76df7c1b2c47700517b6c6ded30323
Matt Auer <mattauer@umich.edu>
2024-08-12 13:38:35 -0500

    fix: flow

--- app-commons/src/components/WelcomePage.tsx
+++ app-commons/src/components/WelcomePage.tsx
@@ -24,59 +24,70 @@ export const WelcomePage = ({
   messageProps,
   setCurrentStep,
 }: WelcomePageProps) => {
-  const { connect } = useConnectUI();
-  const { connectAsync: connectBurner } = useConnect();
-  const { refetch: refetchWallet, wallet } = useWallet();
-  const { balance } = useBalance({ address: wallet?.address.toString() });
+  const { connect: connectUI } = useConnectUI();
+  const { connect: connectBurner } = useConnect();
+  const {
+    wallet,
+    isLoading: isLoadingWallet,
+    isPending: isPendingWallet,
+    isFetching: isFetchingWallet,
+  } = useWallet();
+  const {
+    balance,
+    isFetching: isFetchingBalance,
+    isPending: isPendingBalance,
+    isLoading: isLoadingBalance,
+  } = useBalance({
+    address: wallet?.address.toString(),
+  });
   const { isConnected } = useIsConnected();
-  const [redirect, setRedirect] = useState(false);
 
-  // useEffect(() => {
-  //   if (isConnected &&  !balance?.gt(0)) {
-  //     setCurrentStep(CurrentStep.Faucet);
-  //   }
-  // }, [isConnected, balance]);
-    useEffect(() => {
-    if (isConnected && wallet && !balance?.gt(0) && redirect) {
-      window.location.href = `https://faucet-testnet.fuel.network/?address=${wallet.address.toString()}&redirectUrl=${window.location.href}`;
+  const isBalanceLoading =
+    isFetchingBalance || isPendingBalance || isLoadingBalance;
+  const isWalletLoading =
+    isLoadingWallet || isPendingWallet || isFetchingWallet;
+  const isLoading = isBalanceLoading || isWalletLoading;
+
+  useEffect(() => {
+    if (isConnected && wallet) {
+      if (balance && balance.gt(0)) {
+        setCurrentStep(CurrentStep.Success);
+      } else if (!isBalanceLoading) {
+        const redirectUrl = new URL("https://faucet-testnet.fuel.network/");
+        redirectUrl.searchParams.append("address", wallet.address.toString());
+        redirectUrl.searchParams.append("redirectUrl", window.location.href);
+        window.location.href = redirectUrl.href;
+      }
     }
-  }, [isConnected, wallet, redirect]);
+  }, [isConnected, wallet, balance, isBalanceLoading]);
 
   return (
     <Stack spacing={3} className="w-5/6 items-center">
       {message && <Text {...messageProps}>{message}</Text>}
-      <Button
-        className="btn-primary h-12 w-full"
-        onClick={async () => {
-          const isConnected = await connectBurner("Burner Wallet");
-          if (isConnected) {
-            const { data: wallet } = await refetchWallet();
-            if (wallet) {
-              window.open(
-                `https://faucet-testnet.fuel.network/?address=${wallet.address.toString()}&autoClose`,
-                "_blank"
-              );
-            } else {
-              toast.error("Error fetching wallet");
-            }
-          } else {
-            toast.error("Error connecting wallet");
-          }
-        }}
-      >
-        Temporary Wallet
-      </Button>
-      <Box className="border-b-2 border-slate-600 w-full" />
-      <Button
-        variant="outlined"
-        className="text-white h-12 w-full border-slate-600"
-        onClick={() => {
-          connect();
-          setRedirect(true);
-        }}
-      >
-        Connect
-      </Button>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Button
+            className="btn-primary h-12 w-full"
+            onClick={() => {
+              connectBurner("Burner Wallet");
+            }}
+          >
+            Temporary Wallet
+          </Button>
+          <Box className="border-b-2 border-slate-600 w-full" />
+          <Button
+            variant="outlined"
+            className="text-white h-12 w-full border-slate-600"
+            onClick={() => {
+              connectUI();
+            }}
+          >
+            Connect
+          </Button>
+        </>
+      )}
     </Stack>
   );
 };
