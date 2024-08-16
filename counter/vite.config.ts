73fa45f630969cc2829a556c727bc3631a40f382
rishabhkeshan <rishabhkeshan@gmail.com>
2024-08-17 00:05:49 +0530

    feat: added changes to deployment scripts

--- counter/vite.config.ts
+++ counter/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env": env,
+export default defineConfig({
+  base: process.env.COUNTER_BASE_URL,
+  build: {
+    outDir: process.env.COUNTER_DIST,
+  },
+  plugins: [react(), vercel()],
+  // @ts-ignore
+  resolve: {
+    alias: {
+      src: "/src",
+      hooks: "/src/hooks",
+      lib: "/src/lib",
     },
-    plugins: [react()],
-  };
+  },
 });
