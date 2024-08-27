993ca5e7e95025c9e7a34209cdd321c3dbcf885c
Matt Auer <mattauer@umich.edu>
2024-08-27 14:52:20 -0500

    ci: fix preview

--- nft/vite.config.ts
+++ nft/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sway-examples/',
+  base: process.env.BASE_URL,
   build: {
     outDir: process.env.NFT_DIST,
     minify: false,
