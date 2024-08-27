c839de6a49fbd2e701d7df5616ea8ccaffbd73a6
Matt Auer <mattauer@umich.edu>
2024-08-27 17:55:58 -0500

    debugging

--- nft/vite.config.ts
+++ nft/vite.config.ts
@@ -5,6 +5,7 @@ console.log(`process.env.VITE_BASE_URL`, process.env.VITE_BASE_URL);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "../",
   base: process.env.VITE_BASE_URL,
   build: {
     outDir: process.env.NFT_DIST,
