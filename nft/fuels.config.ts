f8e06e051e7bb54abc5f662e1b0223fd436b0670
Matt Auer <mattauer@umich.edu>
2024-09-13 14:06:56 -0500

    fix: build

--- nft/fuels.config.ts
+++ nft/fuels.config.ts
@@ -16,7 +16,9 @@ const fuelCorePort =
   +(process.env.NEXT_PUBLIC_FUEL_NODE_PORT as string) || 4000;
 const NODE_URL =
   process.env.VITE_PUBLIC_DAPP_ENVIRONMENT === "local"
-    ? `http://127.0.0.1:${env.VITE_PUBLIC_FUEL_NODE_PORT || 4000}/v1/graphql`
+    ? `http://127.0.0.1:${
+        process.env.VITE_PUBLIC_FUEL_NODE_PORT || 4000
+      }/v1/graphql`
     : "https://testnet.fuel.network/v1/graphql";
 
 export default createConfig({
