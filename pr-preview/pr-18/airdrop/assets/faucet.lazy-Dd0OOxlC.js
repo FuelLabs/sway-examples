a9f57e6f380391504fa0ebd27a48ec91d4c08f1a
Nazeeh21 <Nazeeh21@users.noreply.github.com>
2024-10-29 12:20:17 +0000

    Deploy preview for PR 18 🛫

---
+++ pr-preview/pr-18/airdrop/assets/faucet.lazy-Dd0OOxlC.js
@@ -0,0 +1,29 @@
+import { c as createLazyFileRoute, u as useFaucet, a as useActiveWallet, r as reactExports, j as jsxRuntimeExports, C as CURRENT_ENVIRONMENT, E as Environments, T as TESTNET_FAUCET_LINK } from "./index-GM8AQeMh.js";
+const Route = createLazyFileRoute("/faucet")({
+  component: Index
+});
+function Index() {
+  useFaucet();
+  const { wallet, refreshWalletBalance } = useActiveWallet();
+  const [receiverAddress, setReceiverAddress] = reactExports.useState("");
+  reactExports.useState("5");
+  reactExports.useEffect(() => {
+    if (wallet) {
+      setReceiverAddress(wallet.address.toB256());
+    }
+  }, [wallet]);
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+    CURRENT_ENVIRONMENT === Environments.LOCAL,
+    /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+      "iframe",
+      {
+        src: wallet ? `${TESTNET_FAUCET_LINK}?address=${wallet.address.toB256()}` : TESTNET_FAUCET_LINK,
+        title: "faucet",
+        className: "w-full h-screen overflow-scroll"
+      }
+    ) })
+  ] });
+}
+export {
+  Route
+};
---
+++ pr-preview/pr-18/airdrop/assets/index.lazy-BByO9ic7.js
@@ -0,0 +1,47 @@
+import { c as createLazyFileRoute, j as jsxRuntimeExports, b as useNavigate, r as reactExports, B as Button } from "./index-GM8AQeMh.js";
+import { T as Text } from "./Text-9DwczbZD.js";
+import { u as useGetAirdropData, G as Grid, H as HomeCard } from "./HomeCard-DMZyQJUn.js";
+const Route = createLazyFileRoute("/airdrop/")({
+  component: () => /* @__PURE__ */ jsxRuntimeExports.jsx(Airdrop, {})
+});
+function Airdrop() {
+  const { data: airdropData, isFetching, isError } = useGetAirdropData();
+  const navigate = useNavigate();
+  reactExports.useEffect(() => {
+    console.log("airdropData: ", airdropData);
+  }, [airdropData, isFetching]);
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { children: [
+    isFetching && /* @__PURE__ */ jsxRuntimeExports.jsx(Text, { variant: "h4", sx: { paddingBottom: "28px", width: "full" }, children: "Fetching Airdrop ContractIds..." }),
+    isError && /* @__PURE__ */ jsxRuntimeExports.jsx(Text, { variant: "h4", sx: { paddingBottom: "28px", width: "full" }, children: "Error fetching Airdrop ContractIds" }),
+    !isFetching && !isError && airdropData && /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { className: "min-h-screen overflow-y-auto items-center p-20 flex flex-col gap-6", children: [
+      /* @__PURE__ */ jsxRuntimeExports.jsx(
+        Button,
+        {
+          onClick: () => navigate({
+            // to:  VITE_BASE_URL + "/airdrop/create",
+            to: "/airdrop/create"
+          }),
+          children: "Create New Airdrop"
+        }
+      ),
+      /* @__PURE__ */ jsxRuntimeExports.jsx(Text, { variant: "h4", sx: { paddingBottom: "28px", width: "full" }, children: "Below are the open Airdrops" }),
+      /* @__PURE__ */ jsxRuntimeExports.jsx(Grid, { container: true, overflow: "auto", spacing: 3, children: airdropData == null ? void 0 : airdropData.map(({ contractId, recipients }, index) => /* @__PURE__ */ jsxRuntimeExports.jsx(Grid, { className: "m-3", children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+        HomeCard,
+        {
+          title: "Airdrop " + (index + 1),
+          href: `/airdrop/claim/${contractId}?recipient=${JSON.stringify(
+            recipients
+          )}`,
+          children: /* @__PURE__ */ jsxRuntimeExports.jsxs(Text, { children: [
+            contractId.toString().slice(0, 10),
+            "....",
+            contractId.toString().slice(-3)
+          ] }, index)
+        }
+      ) })) })
+    ] })
+  ] });
+}
+export {
+  Route
+};
