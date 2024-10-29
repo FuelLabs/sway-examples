b621f52862a1714fd6bca495d449c7044368de13
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2024-10-29 17:25:59 +0530

    Update lib.ts

--- airdrop/src/lib.ts
+++ airdrop/src/lib.ts
@@ -7,13 +7,25 @@ export const Environments = {
 } as const;
 type Environment = (typeof Environments)[keyof typeof Environments];
 
+const getEnv = () => {
+  try {
+    if (import.meta.env) {
+      return import.meta.env;
+    }
+    return process.env;
+  } catch {
+    return process.env;
+  }
+};
+
+const env = getEnv();
 /**
  * The current environment is determined by the
  * `VITE_DAPP_DAPP_ENVIRONMENT` environment variable.
  * If it's not set, the default is `local`.
  */
-export const CURRENT_ENVIRONMENT: Environment = "testnet"
-  // (process.env.VITE_DAPP_DAPP_ENVIRONMENT as Environment) || Environments.LOCAL;
+export const CURRENT_ENVIRONMENT: Environment = "testnet";
+// (process.env.VITE_DAPP_DAPP_ENVIRONMENT as Environment) || Environments.LOCAL;
 
 const TESTNET_NETWORK_URL = "https://testnet.fuel.network/v1/graphql";
 // The node URL is determined by the current environment too.
@@ -31,7 +43,7 @@ export interface AppWallet {
 export const TESTNET_FAUCET_LINK = "https://faucet-testnet.fuel.network/";
 
 export const FAUCET_LINK =
-// @ts-expect-error will fix it once the build succeeds
+  // @ts-expect-error will fix it once the build succeeds
   CURRENT_ENVIRONMENT === Environments.LOCAL ? "/faucet" : TESTNET_FAUCET_LINK;
 
 export const FAUCET_PRIVATE_KEY = "0x01";
@@ -44,7 +56,7 @@ export const DOCS_URL = "https://docs.fuel.network";
 
 export const GATEWAY_URL = "https://beige-ready-porcupine-957.mypinata.cloud";
 
-export const PINATA_JWT = process.env.VITE_PINATA_JWT;
+export const PINATA_JWT = env.VITE_PINATA_JWT;
 
 export const PINATA_API_URL = "https://api.pinata.cloud";
 
