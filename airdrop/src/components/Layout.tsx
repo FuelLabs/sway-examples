5f9f0caddab77e78607d147f882e28105e48883e
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2024-12-02 12:11:52 +0530

    Update provider and dependencies

--- airdrop/src/components/Layout.tsx
+++ airdrop/src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import { Toaster } from "react-hot-toast";
 import { NavMenu } from "./NavMenu";
+import { OnboardingFlow } from "sway-example-commons";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-  
   return (
     <div className="w-full">
       <Toaster />
+      <OnboardingFlow
+        welcomeMessage={<div>Welcome to the Airdrop!</div>}
+        container={() => document.getElementById("root")}
+      />
       <div className="flex flex-col w-['100vw'] bg-black text-white">
         <NavMenu />
 
--- airdrop/src/routes/__root.tsx
+++ airdrop/src/routes/__root.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { Layout } from "../components/Layout";
-import { useEffect, useMemo, useState } from "react";
-import { Provider } from "fuels";
-import { NODE_URL } from "../lib";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { FuelProvider } from "@fuels/react";
+import { DEFAULT_WAGMI_CONFIG } from "@/config/config";
 import {
-  BakoSafeConnector,
   BurnerWalletConnector,
   FuelWalletConnector,
   FuelWalletDevelopmentConnector,
   FueletWalletConnector,
   WalletConnectConnector,
-  SolanaConnector,
 } from "@fuels/connectors";
+import { FuelProvider, type NetworkConfig, type UIConfig } from "@fuels/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { coinbaseWallet, walletConnect } from "@wagmi/connectors";
+import type { Config as WagmiConfig } from "@wagmi/core";
+import { createConfig, http, injected } from "@wagmi/core";
+import { mainnet, sepolia } from "@wagmi/core/chains";
+import { CHAIN_IDS, Provider } from "fuels";
+import { useEffect, useMemo, useState } from "react";
+import { OnboardingFlowProvider } from "sway-example-commons";
+import { Layout } from "../components/Layout";
 import { ActiveWalletProvider } from "../hooks/useActiveWallet";
-import { OnboardingFlow, OnboardingFlowProvider } from "sway-example-commons";
+import { NODE_URL, WC_PROJECT_ID } from "../lib";
 
 /**
  * react-query is a peer dependency of @fuels/react, so we set it up here.
@@ -32,7 +35,7 @@ export const Route = createRootRoute({
      * Create a Provider instance.
      * We memoize it to avoid creating a new instance on every render.
      */
-    const providerToUse = useMemo(() => Provider.create(NODE_URL), [NODE_URL]);
+    const providerToUse = useMemo(() => Provider.create(NODE_URL), []);
 
     useEffect(() => {
       setIsMounted(true);
@@ -41,43 +44,70 @@ export const Route = createRootRoute({
     // Only render the component if the page has been mounted.
     if (!isMounted) return null;
 
+    const UI_CONFIG: UIConfig = { suggestBridge: false };
+
+    const METADATA = {
+      name: "Airdrop App",
+      description: "Airdrop Assets to multiple addresses",
+      url: location.href,
+      icons: ["https://connectors.fuel.network/logo_white.png"],
+    };
+
+    const wagmiConfig: WagmiConfig = createConfig({
+      chains: [mainnet, sepolia],
+      transports: {
+        [mainnet.id]: http(),
+        [sepolia.id]: http(),
+      },
+      connectors: [
+        injected({ shimDisconnect: false }),
+        walletConnect({
+          projectId: WC_PROJECT_ID,
+          metadata: METADATA,
+          showQrModal: false,
+        }),
+        coinbaseWallet({
+          appName: METADATA.name,
+          appLogoUrl: METADATA.icons[0],
+          darkMode: true,
+          reloadOnDisconnect: true,
+        }),
+      ],
+    });
+
+    const NETWORKS_ONLY_CURRENT: NetworkConfig[] = [
+      {
+        url: "https://testnet.fuel.network/v1/graphql",
+        chainId: CHAIN_IDS.fuel.testnet,
+      },
+    ];
+
     return (
       <>
         <QueryClientProvider client={queryClient}>
           <FuelProvider
             fuelConfig={{
-              /**
-               * The list of wallet connectors.
-               * You can add or remove connectors from here based on your needs.
-               * See https://wallet.fuel.network/docs/dev/connectors/
-               */
               connectors: [
                 new FuelWalletConnector(),
-                new BurnerWalletConnector({
-                  fuelProvider: providerToUse,
-                }),
+                new FueletWalletConnector(),
                 new WalletConnectConnector({
                   fuelProvider: providerToUse,
+                  wagmiConfig: DEFAULT_WAGMI_CONFIG,
+                  projectId: WC_PROJECT_ID,
                 }),
-                new BakoSafeConnector(),
-                new FueletWalletConnector(),
                 new FuelWalletDevelopmentConnector(),
-                new SolanaConnector({
-                  fuelProvider: providerToUse,
-                }),
+                new BurnerWalletConnector({ fuelProvider: providerToUse }),
               ],
             }}
+            uiConfig={UI_CONFIG}
+            networks={NETWORKS_ONLY_CURRENT}
           >
             <ActiveWalletProvider>
-              <Layout>
-                {/* <OnboardingFlowProvider> */}
-                  {/* <OnboardingFlow
-                    welcomeMessage={<div>Welcome to the Airdrop!</div>}
-                    container={() => document.getElementById("root")}
-                  /> */}
+              <OnboardingFlowProvider>
+                <Layout>
                   <Outlet />
-                {/* </OnboardingFlowProvider> */}
-              </Layout>
+                </Layout>
+              </OnboardingFlowProvider>
             </ActiveWalletProvider>
           </FuelProvider>
         </QueryClientProvider>
