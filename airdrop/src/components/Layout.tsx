41f49b03c774b31f003d65c63a8d68f6d68d298b
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2024-12-17 19:42:31 +0530

    Add `sway-example-commons` package

--- airdrop/src/components/Layout.tsx
+++ airdrop/src/components/Layout.tsx
@@ -1,17 +1,26 @@
 import { Toaster } from "react-hot-toast";
 import { NavMenu } from "./NavMenu";
+import { OnboardingFlow, OnboardingFlowProvider } from "sway-example-commons";
+import { useFuel } from "@fuels/react";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
-  
+  const { networks } = useFuel();
+  console.log("networks", networks);
   return (
-    <div className="w-full">
-      <Toaster />
-      <div className="flex flex-col w-['100vw'] bg-background text-foreground">
-        <NavMenu />
-        <div className="min-h-screen items-center p-24 flex flex-col gap-6">
-          {children}
+    // <OnboardingFlowProvider>
+      <div className="w-full">
+        {/* <OnboardingFlow
+          welcomeMessage={<div>Welcome to the Airdrop Dapp!</div>}
+          container={() => document.getElementById("root")}
+        /> */}
+        <Toaster />
+        <div className="flex flex-col w-['100vw'] bg-background text-foreground">
+          <NavMenu />
+          <div className="min-h-screen items-center p-24 flex flex-col gap-6">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    // </OnboardingFlowProvider>
   );
 };
